bae582e33459b0a5ede87123a2e586e7c41e55e9
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-08-16 01:21:44 +0600

    get and set element value using a function

--- js/deposit.js
+++ js/deposit.js
@@ -1,17 +1,3 @@
-function getInputFieldValueById(inputFieldId) {
-    const inputField = document.getElementById(inputFieldId);
-    const inputFieldValueString = inputField.value;
-    const inputFieldValue = parseFloat(inputFieldValueString);
-    inputField.value = "";
-    return inputFieldValue;
-}
-
-function getElementValueById(elementId) {
-    const element = document.getElementById(elementId);
-    const elementValueString = element.innerText;
-    const value = parseFloat(elementValueString);
-    return value;
-}
 document.getElementById('btn-deposit').addEventListener('click', function () {
     /* 
     1. get the element by id
@@ -19,4 +5,19 @@ document.getElementById('btn-deposit').addEventListener('click', function () {
     3. convert string value to a number
     */
     const newDepositAmount = getInputFieldValueById('deposit-field');
+    /* 
+    1. get previous deposit total by id
+    */
+    const previousDepositTotal = getTextElementValueById('deposit-total')
+
+    // calculate new deposit total
+    const newDepositTotal = previousDepositTotal + newDepositAmount;
+    // set deposit total value
+    setTextElementValueById('deposit-total', newDepositTotal);
+
+    // get previous balance by using the function
+    const previousBalanceTotal = getInputFieldValueById('balance-total');
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    setTextElementValueById('balance-total', newBalanceTotal);
+
 })
